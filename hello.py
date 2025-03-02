c553d6bef23edfe115994da39b84545447d0eb5a
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-03-03 00:59:34 +0100

    vs code learn

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
+from datetime import date
+
 def say_hello(name):
     print("Hello, ", name)
 
 say_hello("Daniel")
+
+def say_day_of_week(date):
+    print("Today is ", date)
+
+say_day_of_week(date.today())
